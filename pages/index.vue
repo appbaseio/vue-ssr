b7cc7992ad198d9723e80791dd00256292cd05e0
bietkul <kuldepsaxena155@gmail.com>
2023-02-02 17:48:03 +0530

    fix: search config

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,15 @@
+<script setup>
+import { getServerState } from "@appbaseio/reactivesearch-vue";
+import Search from "../components/search.vue";
+const route = useRoute();
+const { data, pending, error, refresh } = await useAsyncData(() =>
+  getServerState(Search, route.query)
+);
+const initialState = data.value;
+</script>
 
 <template>
   <div>
-    <Search 
-      :initial-state="initialState" 
-      :context-collector="contextCollector" />
+    <search :initial-state="initialState" />
   </div>
 </template>
-
-
-<script>
-import { getServerState } from '@appbaseio/reactivesearch-vue';
-import Search from '../components/search.vue';
-
-export default defineNuxtComponent({
-	async asyncData() {
-		const route = useRoute();
-		try {
-			const initialState = await getServerState(Search, route.query);
-			return {
-				initialState,
-			};
-		} catch (e) {
-			console.error('error', e);
-			return { error: e };
-		}
-	},
-});
-</script>
